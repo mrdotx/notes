8f9629b673d0bd903ac02c13dc5fd6413550b328
mrdotx <klassiker@gmx.de>
2026-02-17 06:07:15 +0100

    colors are only used in the interactive shell added

--- build.sh
+++ build.sh
@@ -3,17 +3,18 @@
 # path:   /home/klassiker/.local/share/repos/notes/build.sh
 # author: klassiker [mrdotx]
 # url:    https://github.com/mrdotx/notes
-# date:   2025-08-15T03:43:58+0200
+# date:   2026-02-17T05:58:31+0100
 
 # config
 notes="$HOME/.local/share/repos/notes"
 destination_path="/srv/http/notes/"
 
-# color variables
-reset="\033[0m"
-bold="\033[1m"
-blue="\033[94m"
-cyan="\033[96m"
+# color variables for interactive shell
+tty -s \
+    && reset="\033[0m" \
+    && bold="\033[1m" \
+    && blue="\033[94m" \
+    && cyan="\033[96m"
 
 # help
 script=$(basename "$0")
